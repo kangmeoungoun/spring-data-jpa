b8f515aabbc4dbd167b403b6a9c39a956aa73854
kangmeoungoun <kes98202@naver.com>
2020-12-11 21:56:41 +0900

    test

---
+++ src/test/java/me/goldapple/demospringdata/DemospringdataApplicationTests.java
@@ -0,0 +1,13 @@
+package me.goldapple.demospringdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
